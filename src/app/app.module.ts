45131af4d10eae17e5fc26ec3937354fd3e16636
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2018-11-09 01:49:43 +0530

    git commit -m "upload image"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//adding camera module
+import { Camera } from '@ionic-native/camera';
+
+//file module
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+//http
+import { HTTP } from '@ionic-native/http';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +34,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Camera,FileTransfer, File,HTTP
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController } from 'ionic-angular';
+import { Camera,CameraOptions} from '@ionic-native/camera';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { HTTP } from '@ionic-native/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  pic:any;
+  img:any;
 
+  constructor(public navCtrl: NavController,
+    private camera:Camera,
+    private file: File,
+    private loadingCtrl:LoadingController,
+    private transfer: FileTransfer,
+    private http:HTTP
+    ) {}
+
+  pickPhoto(){
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+        saveToPhotoAlbum:false
+      }
+  
+      this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        this.pic = 'data:image/jpeg;base64,' + imageData;
+        this.img = imageData;
+      }, (err) => {
+        // Handle error
+      });
+    }
+
+
+  uploadImage(){
+    this.http.put('http://192.168.8.102:5000/upload', {"the_file":this.img}, {})
+  .then(data => {
+    console.log(data.status);
+    console.log(data.data); // data received by server
+    console.log(data.headers);
+
+  })
+  .catch(error => {
+
+    console.log(error.status);
+    console.log(error.error); // error message as string
+    console.log(error.headers);
+
+  });
+    
   }
+
+
+
 
 }
